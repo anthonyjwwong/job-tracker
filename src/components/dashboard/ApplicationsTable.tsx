1b6a1146a75226a165f6daa42bc8f3b208d0b85d
Anthony <anthonywongjw@gmail.com>
2026-06-28 09:18:44 -0400

    feat: add pagination to applications table

--- src/components/dashboard/ApplicationsTable.tsx
+++ src/components/dashboard/ApplicationsTable.tsx
@@ -14,7 +14,7 @@ import { Application, ApplicationStatus } from "@/generated/prisma/client";
 import { useRouter } from "next/navigation";
 import { needsFollowUp } from "@/lib/utils";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
-
+import { useState } from "react";
 type Props = {
   filters: "ALL" | ApplicationStatus;
   applications: Application[];
@@ -31,6 +31,20 @@ const ApplicationsTable = ({ applications, filters }: Props) => {
       ? applications
       : applications.filter((app) => app.currentStatus === filters);
 
+  const ITEMS_PER_PAGE = 15;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+
+  const paginatedData = currentFilter.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(currentFilter.length / ITEMS_PER_PAGE);
+  const handlePageChange = (newPage: number) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
   return (
     <div className="mt-5">
       <Table>
@@ -44,7 +58,7 @@ const ApplicationsTable = ({ applications, filters }: Props) => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {currentFilter.map((app) => (
+          {paginatedData.map((app) => (
             <TableRow
               key={app.id}
               className="cursor-pointer"
@@ -82,6 +96,26 @@ const ApplicationsTable = ({ applications, filters }: Props) => {
           ))}
         </TableBody>
       </Table>
+
+      {/* Pagination */}
+      <div className="text-center my-4 text-gray-600">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Next
+        </button>
+
+        <span className="mx-6">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Previous
+        </button>
+      </div>
     </div>
   );
 };
